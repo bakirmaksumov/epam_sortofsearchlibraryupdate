540154c27ac63c491374f192f109d599f4ed5e0a
bakirmaksumov <30586761+bakirmaksumov@users.noreply.github.com>
2021-08-04 18:47:11 +0500

    initial commit

---
+++ SortByIndexOfLibrary/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortByIndexOfLibrary
+{
+   public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool Equals(Product otherProduct)
+        {
+            //return true;
+            if (otherProduct == null)
+            {
+                return false;
+            }
+
+            return this.Name.Equals(otherProduct.Name);
+
+        }
+
+    }
+}
---
+++ SortByIndexOfLibrary/SortIndexLibrary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SortByIndexOfLibrary
+{
+    public static class SortIndexLibrary
+    {
+        public static void Sort(int[] numbers)
+        {
+            if (numbers != null)
+            {
+                int temp = 0;
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    for (int i = 0; i < numbers.Length - 1; i++)
+                    {
+                        if (numbers[i] > numbers[i + 1])
+                        {
+                            temp = numbers[i]; // Interchanging of array's indexes
+                            numbers[i] = numbers[i + 1];
+                            numbers[i + 1] = temp;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+           
+        }
+
+        public static int IndexOf(Product[] products, Predicate<Product> predicate)
+        {
+            if (products != null && predicate!=null)
+            {
+                for (int i = 0; i < products.Length; i++)
+                {
+                    var product = products[i];
+                    if (predicate(product))
+                    {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}
